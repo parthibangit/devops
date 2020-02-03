af454b912838cd91f7cb44616976468fa5085f30
c049977 <c049977@WV-MN14GLAE629X.us.bank-dns.com>
2020-02-03 12:18:34 +0530

    Test commit

---
+++ src/test/java/basicTest/GitClass.java
@@ -0,0 +1,15 @@
+package basicTest;
+
+public class GitClass 
+{
+	
+	// This is a test comment
+	   
+
+	public static void main(String[] args) {
+		
+		System.out.println("Heloo world");
+
+	}
+
+}
